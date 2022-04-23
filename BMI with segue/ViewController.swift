fb06e3e572ef0d1e2102426bcc40f0bafedb76c8
Zhanagul <190107044@stu.sdu.edu.kz>
2022-04-23 14:24:45 +0600

    Initial Commit

---
+++ BMI with segue/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMI with segue
+//
+//  Created by Zhanagul on 23.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
